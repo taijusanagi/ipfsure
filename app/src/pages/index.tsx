61551f992ec1597f0bb567384ecebd21be5eb6e9
taijusanagi <taijusanagi@gmail.com>
2023-09-08 16:06:15 +0700

    added frontend flow

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -9,10 +9,35 @@ export default function HomePage() {
   const [cid, setCID] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalMode, setModalMode] = useState<
-    "select" | "nftstorage" | "nftstorage-confirm" | "lighthouse"
+    | "select"
+    | "nftstorage"
+    | "nftstorage-confirm"
+    | "lighthouse"
+    | "lighthouse-confirm"
   >("nftstorage");
   const [nftStorageAPIKey, setNftStorageAPIKey] = useState("");
   const [lighthouseAPIKey, setLighthouseAPIKey] = useState("");
+  const [endDate, setEndDate] = useState(
+    new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString().split("T")[0] // 1 week from now
+  );
+  const [replicationTarget, setReplicationTarget] = useState(2);
+  const [epochs, setEpochs] = useState(4);
+
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setSelectedOptions((prevSelectedOptions) => {
+      if (prevSelectedOptions.includes(value)) {
+        // If the option is already selected, filter it out
+        return prevSelectedOptions.filter((option) => option !== value);
+      } else {
+        // If the option is not selected, add it to the state
+        return [...prevSelectedOptions, value];
+      }
+    });
+  };
 
   return (
     <div
@@ -175,9 +200,7 @@ export default function HomePage() {
                     alt="NFTStorage Logo"
                     className="mx-auto h-400 w-auto mb-4 p-12 sm:p-24"
                   />
-                  <p className="mb-2">
-                    Now your content is backup in NFTStorage.
-                  </p>
+                  <p className="mb-2">Content backed up in NFTStorage.</p>
                   <p>
                     <a
                       href="#"
@@ -198,7 +221,7 @@ export default function HomePage() {
                 </h2>
                 <div className="mb-4">
                   <label
-                    htmlFor="name"
+                    htmlFor="cid"
                     className="block text-black text-sm font-bold mb-2"
                   >
                     CID
@@ -207,18 +230,143 @@ export default function HomePage() {
                 </div>
                 <div className="mb-4">
                   <label
-                    htmlFor="name"
+                    htmlFor="lighthouse-api-key"
                     className="block text-black text-sm font-bold mb-2"
                   >
                     Input Lighthouse API Key
                   </label>
                   <input
                     type="text"
+                    id="lighthouse-api-key"
                     placeholder=""
                     value={lighthouseAPIKey}
-                    onChange={(e) => setNftStorageAPIKey(e.target.value)}
+                    onChange={(e) => setLighthouseAPIKey(e.target.value)}
                     className="shadow appearance-none border rounded w-full p-3 text-black leading-tight focus:outline-none text-xs"
                   />
+                </div>
+                <div className="mb-4 space-y-2">
+                  <label className="text-black text-sm font-bold mb-2">
+                    Advanced Options:
+                  </label>
+                  <div>
+                    <input
+                      type="checkbox"
+                      value="replication"
+                      checked={selectedOptions.includes("replication")}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="replication" className="ml-2">
+                      Replication
+                    </label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      value="repair"
+                      checked={selectedOptions.includes("repair")}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="repair" className="ml-2">
+                      Repair
+                    </label>
+                  </div>
+                  <div>
+                    <input
+                      type="checkbox"
+                      value="renewal"
+                      checked={selectedOptions.includes("renewal")}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="renewal" className="ml-2">
+                      Renewal
+                    </label>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="endDate"
+                    className="block text-black text-sm font-bold mb-2"
+                  >
+                    End Date:
+                  </label>
+                  <input
+                    type="date"
+                    id="endDate"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                    disabled={selectedOptions.length === 0}
+                    className="shadow appearance-none border rounded w-full p-3 text-black leading-tight focus:outline-none text-xs disabled:opacity-50 disabled:cursor-not-allowed"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="replicationTarget"
+                    className="block text-black text-sm font-bold mb-2"
+                  >
+                    Replication Target:
+                  </label>
+                  <input
+                    type="number"
+                    id="replicationTarget"
+                    value={replicationTarget}
+                    onChange={(e) =>
+                      setReplicationTarget(Number(e.target.value))
+                    }
+                    disabled={selectedOptions.length === 0}
+                    className="shadow appearance-none border rounded w-full p-3 text-black leading-tight focus:outline-none text-xs disabled:opacity-50 disabled:cursor-not-allowed"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="epochs"
+                    className="block text-black text-sm font-bold mb-2"
+                  >
+                    Epochs:
+                  </label>
+                  <input
+                    type="number"
+                    id="epochs"
+                    value={epochs}
+                    onChange={(e) => setEpochs(Number(e.target.value))}
+                    disabled={selectedOptions.length === 0}
+                    className="shadow appearance-none border rounded w-full p-3 text-black leading-tight focus:outline-none text-xs disabled:opacity-50 disabled:cursor-not-allowed"
+                  />
+                </div>
+                <div className="flex items-center justify-end">
+                  <button
+                    disabled={!cid}
+                    onClick={() => {
+                      setModalMode("lighthouse-confirm");
+                    }}
+                    className="bg-gradient-to-br from-gray-700 to-black text-white py-2 px-4 rounded hover:opacity-80 focus:outline-none transition-opacity disabled:opacity-50 disabled:cursor-not-allowed hover:enabled:opacity-75"
+                  >
+                    Start
+                  </button>
+                </div>
+              </div>
+            )}
+            {modalMode === "lighthouse-confirm" && (
+              <div>
+                <div className="text-center">
+                  <h2 className="text-lg font-bold mb-4">
+                    Lighthouse Backup Confirmed
+                  </h2>
+                  <img
+                    src="/logo-lighthouse.png"
+                    alt="Lighthouse Logo"
+                    className="mx-auto h-400 w-auto mb-4 p-12 sm:p-24"
+                  />
+                  <p className="mb-2">Content backed up in Lighthouse.</p>
+                  <p>
+                    <a
+                      href="#"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 hover:text-blue-600"
+                    >
+                      Check it out
+                    </a>
+                  </p>
                 </div>
               </div>
             )}
