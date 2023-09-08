5843953c73a89a4767f98163c9b7dca81a389631
taijusanagi <taijusanagi@gmail.com>
2023-09-08 15:36:03 +0700

    add lighthouse

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -3,15 +3,16 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 import Head from "next/head";
-import { use, useState } from "react";
+import { useState } from "react";
 
 export default function HomePage() {
   const [cid, setCID] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalMode, setModalMode] = useState<
-    "select" | "nftstorage" | "nftstorage-confirm" | "custom"
+    "select" | "nftstorage" | "nftstorage-confirm" | "lighthouse"
   >("nftstorage");
   const [nftStorageAPIKey, setNftStorageAPIKey] = useState("");
+  const [lighthouseAPIKey, setLighthouseAPIKey] = useState("");
 
   return (
     <div
@@ -20,23 +21,20 @@ export default function HomePage() {
       <Head>
         <title>IPFSure</title>
       </Head>
-
       <header className="py-4 px-4 flex justify-end items-center mb-12 sm:mb-24">
         <button className="bg-gradient-to-br from-gray-700 to-black text-white py-2 px-4 rounded hover:opacity-80 focus:outline-none transition-opacity disabled:opacity-50 disabled:cursor-not-allowed hover:enabled:opacity-75">
           Connect Wallet
         </button>
       </header>
-
       <div className="text-center px-4 mb-12 sm:mb-24">
         <h1 className="text-3xl sm:text-4xl text-white mb-4">IPFSure 🙆‍♂️</h1>
         <p className="text-lg sm:text-2xl text-gray-300">
           Ensure IPFS data integrity with replication, repair, and renewal
         </p>
       </div>
-
       <div className="flex-1 px-4 mb-12">
         <div className="flex items-center justify-center">
-          <div className="bg-white p-8 rounded-lg shadow-xl w-96 space-y-4 max-w-xl w-full">
+          <div className="bg-white py-6 px-4 rounded-lg shadow-xl w-96 space-y-4 max-w-xl w-full">
             <div>
               <label
                 htmlFor="name"
@@ -46,8 +44,6 @@ export default function HomePage() {
               </label>
               <input
                 type="text"
-                name="cid"
-                id="cid"
                 placeholder="Qm..."
                 value={cid}
                 onChange={(e) => setCID(e.target.value)}
@@ -69,11 +65,10 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="fixed inset-0 flex items-center justify-center z-50 p-4">
           <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="bg-white p-8 rounded-lg shadow-xl max-w-xl w-full relative z-10">
+          <div className="bg-white py-6 px-4 rounded-lg shadow-xl max-w-xl w-full relative z-10">
             <button
               onClick={() => setIsModalOpen(false)}
               className="absolute top-0 right-0 mx-4 my-2 focus:outline-none text-2xl text-gray-800 transition-opacity hover:opacity-75"
@@ -112,13 +107,13 @@ export default function HomePage() {
                   </div>
                   <div className="flex-1 border p-4 rounded shadow-sm bg-gray-50">
                     <h3 className="text-md font-semibold mb-2 text-center">
-                      Custom
+                      Lighthouse
                     </h3>
                     <p className="text-sm text-center mb-4">
-                      Advanced options for replication, repair, and renewal.
+                      Advanced options for custom replication, repair, renewal.
                     </p>
                     <button
-                      onClick={() => setModalMode("custom")}
+                      onClick={() => setModalMode("lighthouse")}
                       className="w-full bg-gradient-to-br from-gray-700 to-black text-white py-2 px-4 rounded hover:opacity-80 focus:outline-none transition-opacity disabled:opacity-50 disabled:cursor-not-allowed hover:enabled:opacity-75"
                     >
                       Choose
@@ -150,8 +145,6 @@ export default function HomePage() {
                   </label>
                   <input
                     type="text"
-                    name="nftstorage-api-key"
-                    id="nftstorage-api-key"
                     placeholder=""
                     value={nftStorageAPIKey}
                     onChange={(e) => setNftStorageAPIKey(e.target.value)}
@@ -180,7 +173,7 @@ export default function HomePage() {
                   <img
                     src="/logo-nftstorage.svg"
                     alt="NFTStorage Logo"
-                    className="mx-auto h-400 w-auto mb-4 p-12"
+                    className="mx-auto h-400 w-auto mb-4 p-12 sm:p-24"
                   />
                   <p className="mb-2">
                     Now your content is backup in NFTStorage.
@@ -198,10 +191,10 @@ export default function HomePage() {
                 </div>
               </div>
             )}
-            {modalMode === "custom" && (
+            {modalMode === "lighthouse" && (
               <div>
                 <h2 className="text-lg font-bold mb-4 text-center">
-                  Custom Backup
+                  Lighthouse Backup
                 </h2>
                 <div className="mb-4">
                   <label
@@ -212,12 +205,26 @@ export default function HomePage() {
                   </label>
                   <p className="text-xs">{cid}</p>
                 </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="name"
+                    className="block text-black text-sm font-bold mb-2"
+                  >
+                    Input Lighthouse API Key
+                  </label>
+                  <input
+                    type="text"
+                    placeholder=""
+                    value={lighthouseAPIKey}
+                    onChange={(e) => setNftStorageAPIKey(e.target.value)}
+                    className="shadow appearance-none border rounded w-full p-3 text-black leading-tight focus:outline-none text-xs"
+                  />
+                </div>
               </div>
             )}
           </div>
         </div>
       )}
-
       <footer className="text-center py-4">
         <p className="text-white text-xs">
           © 2023 IPFSure. All rights reserved.
